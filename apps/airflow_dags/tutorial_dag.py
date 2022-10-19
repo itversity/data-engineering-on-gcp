37ced0e34a8f9684e859c4b86c574a48ad020723
Durga Gadiraju <durga@skyhive.io>
2022-10-19 17:39:35 -0500

    Airflow Content

---
+++ apps/airflow_dags/tutorial_dag.py
@@ -0,0 +1,42 @@
+
+from datetime import datetime, timedelta
+
+# The DAG object; we'll need this to instantiate a DAG
+from airflow import models
+
+# Operators; we need this to operate!
+from airflow.operators.bash import BashOperator
+
+with models.DAG(
+    'tutorial',
+    # These args will get passed on to each operator
+    # You can override them on a per-task basis during operator initialization
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5)
+    },
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=['example'],
+) as dag:
+
+    # t1 and t2 are examples of tasks created by instantiating operators
+    t1 = BashOperator(
+        task_id='print_date',
+        bash_command='date',
+    )
+
+    t2 = BashOperator(
+        task_id='sleep',
+        depends_on_past=False,
+        bash_command='sleep 5',
+        retries=3,
+    )
+
+    t1 >> t2
